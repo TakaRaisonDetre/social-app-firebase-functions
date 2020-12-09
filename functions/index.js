27b29f1794b9298d82b4bd780439b43960129896
原貴悌 <taka_raison@genkiteinoiMac.local>
2020-12-09 13:34:28 +0900

    added express to the firebase function

--- functions/index.js
+++ functions/index.js
@@ -3,30 +3,29 @@ const admin = require('firebase-admin');
 
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
- exports.helloWorld = functions.https.onRequest((request, response) => {
-   functions.logger.info("Hello logs!", {structuredData: true});
-   response.send("Hello from Firebase!");
- });
+const express = require('express')
+const app = express();
 
- exports.getScreams = functions.https.onRequest((req, res)=>{
-     admin.firestore()
-     .collection('screams')
-     .get()
-     .then(data=>{
-         let screams =[]
-         data.forEach(doc=>{
-             screams.push(doc.data());
-         });
-         return res.json(screams);
-     })
-     .catch(err=>console.error(err));
- });
+app.get('/screams', (req, res)=>{
+    admin.firestore()
+    .collection('screams')
+    .get()
+    .then(data=>{
+        let screams =[]
+        data.forEach(doc=>{
+            screams.push(doc.data());
+        });
+        return res.json(screams);
+    })
+    .catch(err=>console.error(err));
+})
+
+
 
  exports.createScream = functions.https.onRequest((req, res)=>{
-    
+    if(req.method!=='POST'){
+        return res.status(400).json({error: "Method not allowed"});
+    }
     const newScream ={
         body:req.body.body,
         userHandle : req.body.userHandle,
@@ -44,3 +43,5 @@ admin.initializeApp();
         console.error(err)
     })
  }) 
+
+ exports.api = functions.https.onRequest(app);
