b42993621b043548358a6f333c711c509b5d89dc
原貴悌 <taka_raison@genkiteinoiMac.local>
2020-12-09 22:00:05 +0900

    add function to fetch comments on scream

--- functions/index.js
+++ functions/index.js
@@ -4,12 +4,17 @@ const app = require('express')();
 
 const FBAuth = require('./util/fbAuth');
 
-const {getAllScreams, postOneScream} = require('./handlers/scream');
+const {getAllScreams, postOneScream, getScream, commentOnScream} = require('./handlers/scream');
 const {signup, login, uploadImage, addUserDetails, getAuthenticatedUser} = require('./handlers/users')
 
 // scream
 app.get('/screams', getAllScreams)
 app.post('/screams', FBAuth, postOneScream) 
+app.get('/screams/:screamId', getScream );
+// Todo : delete screan
+// Tod like a scream
+// Todo : unlike a scream
+app.post('/screams/:screamId/comment', FBAuth, commentOnScream)
 
  //  users routes
  app.post('/signup', signup);
