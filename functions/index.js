27768fad6570a495749988e7f9263a16feca59b8
原貴悌 <taka_raison@genkiteinoiMac.local>
2020-12-09 16:54:16 +0900

    authorization middleware function is added

--- functions/index.js
+++ functions/index.js
@@ -6,7 +6,7 @@ admin.initializeApp();
 
 
 const firebaseConfig = {
-    
+  
   };
 
 
@@ -35,13 +35,40 @@ app.get('/screams', (req, res)=>{
     .catch(err=>console.error(err));
 })
 
+const FBAuth = (req, res, next )=>{
+    let idToken;
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer ')){
+      idToken = req.headers.authorization.split('Bearer ')[1];
+    } else {
+        console.error('no token found')
+        return res.status(403).json({error: 'Unauthorized'});
+    }
+
+    admin.auth().verifyIdToken(idToken)
+    .then(decodedToken =>{
+        req.user = decodedToken;
+        console.log(decodedToken)
+        return db.collection('users')
+        .where('userId', '==', req.user.uid)
+        .limit(1)
+        .get();
+    })
+    .then(data=>{
+        req.user.handle = data.docs[0].data().handle;
+        return next();
+    })
+    .catch(err=> {
+        console.error('Error while verifying token', err)
+        return res.status(403).json(err);
+    })
+}
 
 
- app.post('/screams', (req, res)=>{
+ app.post('/screams', FBAuth, (req, res)=>{
  
     const newScream ={
         body:req.body.body,
-        userHandle : req.body.userHandle,
+        userHandle : req.user.handle,
         createdAt : new Date().toISOString()
     }
 
