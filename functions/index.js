9482d54dcf1a4892675b757504aef64e747ce6c7
原貴悌 <taka_raison@genkiteinoiMac.local>
2020-12-09 16:12:36 +0900

    login function is added

--- functions/index.js
+++ functions/index.js
@@ -6,7 +6,7 @@ admin.initializeApp();
 
 
 const firebaseConfig = {
-  
+    
   };
 
 
@@ -137,5 +137,33 @@ db.doc(`/users/${newUser.handle}`).get()
 
        
  });
+
+
+ app.post('/login', (req, res)=>{
+     const user = {
+         email: req.body.email,
+         password : req.body.password
+     };
+     let errors ={};
+     if(isEmpty(user.email)) errors.email ='must not be empty';
+     if(isEmpty(user.password)) errors.password ='must not be empty';
+     if(Object.keys(errors).length>0) return res.status(400).json(errors);
+
+     firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+     .then(data=>{
+         return data.user.getIdToken();
+
+     }).then(token=>{
+         return res.json({token})
+     }).catch(err=>{
+         console.error(err);
+         if(err.code ==='auth/wrong-password'){
+             return res.status(403).json({general : 'wrong credential please try again'})
+         } else {
+             return res.status(500).json({error: err.code})
+            }
+        
+     })
+ })
 
  exports.api = functions.https.onRequest(app);
